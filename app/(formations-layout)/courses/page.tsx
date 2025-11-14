0b1d28ba2931e1c99f66df983d3d21517e63f288
Samuel Barubiriza <samuel.barubiriza@abtatsy.com>
2025-11-14 23:01:10 +0100

    using server comp and client comp

--- app/(formations-layout)/courses/page.tsx
+++ app/(formations-layout)/courses/page.tsx
@@ -1,14 +1,40 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { userAgent } from "next/server";
+import Counter from "./counter";
+import { headers } from "next/headers";
+import ServerComponent from "@/components/serverrComponent";
+import { use } from "react";
 
 
-export default function Page() {
+
+export default async function Page() {
+    const userAgentQ = userAgent({
+        headers: await headers()
+    });
+
+    const users = await fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+
+
+
     return <div className="flex flex-col p-4 gap-4 max-w-md mx-auto min-h-full border-x">
 
         <Card>
             <CardHeader>
                 <CardTitle>Tous nos Cours</CardTitle>
+                <CardDescription>{userAgentQ.browser.name}</CardDescription>
+                <Counter>
+
+                    <ServerComponent />
+                </Counter>
+                <ul className="list-disc list-inside">
+                    {users.map(user => <li key={user.name}>{user.name}</li>)}
+                </ul>
             </CardHeader>
 
         </Card>
     </div>
 }
+
---
+++ src/components/serverrComponent.tsx
@@ -0,0 +1,5 @@
+"use server"
+export default async function ServerComponent() {
+
+    return <div>Server Component</div>
+}
