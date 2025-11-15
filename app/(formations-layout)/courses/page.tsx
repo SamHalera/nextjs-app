2f1998fa038608782be55aae67a2e102df357d68
Samuel Barubiriza <samuel.barubiriza@abtatsy.com>
2025-11-15 10:46:47 +0100

    find, update using prisma, server function inside client component

--- app/(formations-layout)/courses/page.tsx
+++ app/(formations-layout)/courses/page.tsx
@@ -1,14 +1,23 @@
 
 import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { userAgent } from "next/server";
 import Counter from "./counter";
 import { headers } from "next/headers";
 import ServerComponent from "@/components/serverrComponent";
 import { use } from "react";
+import { prisma } from "@/lib/prisma";
+import { Star } from "lucide-react";
+import { SelectStar } from "@/components/select-star";
+import { revalidatePath } from "next/cache";
 
-type UserProps = {
-    name: string
+type ReviewsProps = {
+    id: string,
+    name: string,
+    review: string,
+    star: number,
+    createdAt: Date
+    updatedAt: Date
 }
 
 export default async function Page() {
@@ -16,10 +25,20 @@ export default async function Page() {
         headers: await headers()
     });
 
-    const users = await fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
+    const reviews = await prisma.review.findMany();
 
-
+    //function executed on server side
+    const setNewStar = async (reviewsId: string, star: number) => {
+        "use server"
+        console.log("hey")
+        await prisma.review.update({
+            where: { id: reviewsId },
+            data: {
+                star
+            }
+        })
+        revalidatePath('/courses')
+    }
 
     return <div className="flex flex-col p-4 gap-4 max-w-md mx-auto min-h-full border-x">
 
@@ -27,12 +46,25 @@ export default async function Page() {
             <CardHeader>
                 <CardTitle>Tous nos Cours</CardTitle>
                 <CardDescription>{userAgentQ.browser.name}</CardDescription>
-                <Counter>
 
-                    <ServerComponent />
-                </Counter>
+
                 <ul className="list-disc list-inside">
-                    {users.map((user: UserProps) => <li key={user.name}>{user.name}</li>)}
+                    {reviews.map((review: ReviewsProps) => {
+                        return (
+                            <Card key={review.id}>
+                                <CardHeader>
+                                    <CardTitle>{review.name}</CardTitle>
+                                    <div className="flex gap-2 items-center">
+                                        <SelectStar setNewStar={setNewStar.bind(null, review.id)} star={review.star} />
+                                    </div>
+                                </CardHeader>
+                                <CardContent>
+                                    <p>{review.review}</p>
+                                </CardContent>
+
+                            </Card>
+                        )
+                    })}
                 </ul>
             </CardHeader>
 
@@ -40,3 +72,14 @@ export default async function Page() {
     </div>
 }
 
+
+const starsReview = (starNb: number) => {
+
+    let counter = 0;
+    let starsHTML = ""
+    while (counter < starNb) {
+        starsHTML += <Star className="h-4 w-4 text-yellow-500" />
+        counter++
+    }
+    return starsHTML
+}
---
+++ src/components/select-star.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { cn } from "@/lib/utils";
+import { Star } from "lucide-react";
+import { useState } from "react";
+
+
+export const SelectStar = (props: { star: number, setNewStar: (star: number) => void }) => {
+    const [hoverIndex, setHoverIndex] = useState<number | null>(null)
+
+    return (
+        <div className="flex gap-1">
+            {Array.from({ length: 5 }).map((_, i) => {
+                const isFilled = i < props.star;
+                const isNewFilled = hoverIndex ? i < hoverIndex : null
+                return (
+                    <button
+                        onMouseEnter={() => {
+                            setHoverIndex(i)
+                        }}
+                        onMouseLeave={() => {
+                            setHoverIndex(null)
+                        }}
+                        onClick={() => {
+                            props.setNewStar(i + 1)
+                        }}
+                        key={i}>
+                        <Star
+                            className={cn("text-yellow-500 transition cursor-pointer", {
+                                "fill-yellow-500": isFilled,
+                                "fill-yellow-600 translate-y-0.5": isNewFilled
+                            })}
+                            style={{
+                                transitionDelay: `${i * 0.1}s`
+                            }}
+                        />
+                    </button>
+                )
+            })}
+        </div>
+    )
+
+}
