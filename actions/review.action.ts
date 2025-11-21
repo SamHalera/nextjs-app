9e4a44f9456e5a867009918b92309d6d2cc24022
Samuel Barubiriza <samuel.barubiriza@abtatsy.com>
2025-11-21 23:11:56 +0100

    use next safe action and zod

--- actions/review.action.ts
+++ actions/review.action.ts
@@ -1,7 +1,15 @@
 "use server"
 
 import { prisma } from "@/lib/prisma"
+import { actionClient, SafeError } from "@/lib/safe-action-client"
 import { revalidatePath } from "next/cache"
+import { z } from "zod"
+
+
+const reviewSchema = z.object({
+    name: z.string(),
+    review: z.string()
+})
 
 export const addReviewAction = async (formData: FormData) => {
 
@@ -20,3 +28,22 @@ export const addReviewAction = async (formData: FormData) => {
     })
     revalidatePath('/')
 }
+
+export const addReviewSafeAction = actionClient.inputSchema(reviewSchema).action(async ({ parsedInput: input }) => {
+
+    await new Promise((r) => setTimeout(r, 1000))
+
+    if (input.name === "Test") {
+        throw new SafeError('Invalid name!')
+    }
+    const newReview = await prisma.review.create({
+        data: {
+            review: input.review,
+            name: input.name,
+            star: 5
+        }
+    })
+    revalidatePath('/')
+
+    return newReview
+})
--- app/review-form.tsx
+++ app/review-form.tsx
@@ -4,14 +4,22 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 
-import { addReviewAction } from "../actions/review.action"
+import { addReviewSafeAction } from "../actions/review.action"
 import { useFormStatus } from "react-dom"
 import { ComponentProps } from "react"
+import { useAction } from "next-safe-action/hooks"
+import { toast } from "sonner"
 
 export const ReviewForm = () => {
 
 
-    return (<form action={addReviewAction} className="flex flex-col gap-4">
+    const { execute, hasErrored, result, hasSucceeded } = useAction(addReviewSafeAction)
+    return (<form action={async (formData) => {
+        const name = formData.get('name') as string
+        const review = formData.get('review') as string
+        execute({ name, review })
+        toast.success('Review created!')
+    }} className="flex flex-col gap-4">
         <div className="space-y-2">
             <Label htmlFor="name">Review Name</Label>
             <Input id="name" type="name" name="name" />
@@ -21,6 +29,8 @@ export const ReviewForm = () => {
             <Textarea id="review" name="review" />
         </div>
         <SubmitButton type="submit">Submit</SubmitButton>
+        {hasErrored ? <p className="text-red-500">{result.serverError}</p> : null}
+        {hasSucceeded ? <p className="text-green-500">Review created with id : ${result.data?.id}</p> : null}
     </form>)
 }
 
