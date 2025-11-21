95ae52193936c5610be74914ad663a817efc7ee2
Samuel Barubiriza <samuel.barubiriza@abtatsy.com>
2025-11-22 00:07:04 +0100

    using form react-hook-form

--- app/review-form.tsx
+++ app/review-form.tsx
@@ -9,29 +9,90 @@ import { useFormStatus } from "react-dom"
 import { ComponentProps } from "react"
 import { useAction } from "next-safe-action/hooks"
 import { toast } from "sonner"
+import { useRouter } from "next/navigation"
+import z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+
+const FormSchema = z.object({
+    review: z.string().min(10).max(500),
+    name: z.string().min(2).max(50),
+})
 
 export const ReviewForm = () => {
 
-
     const { execute, hasErrored, result, hasSucceeded } = useAction(addReviewSafeAction)
-    return (<form action={async (formData) => {
-        const name = formData.get('name') as string
-        const review = formData.get('review') as string
-        execute({ name, review })
-        toast.success('Review created!')
-    }} className="flex flex-col gap-4">
-        <div className="space-y-2">
-            <Label htmlFor="name">Review Name</Label>
-            <Input id="name" type="name" name="name" />
-        </div>
-        <div className="space-y-2">
-            <Label htmlFor="review">Review</Label>
-            <Textarea id="review" name="review" />
-        </div>
-        <SubmitButton type="submit">Submit</SubmitButton>
-        {hasErrored ? <p className="text-red-500">{result.serverError}</p> : null}
-        {hasSucceeded ? <p className="text-green-500">Review created with id : ${result.data?.id}</p> : null}
-    </form>)
+    const router = useRouter()
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            review: "",
+            name: ""
+        }
+    })
+
+    async function onSubmit(values: z.infer<typeof FormSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+
+        execute(values)
+        router.refresh()
+        form.reset()
+    }
+
+    const updateReview = async (obj: { name: string, review: string }) => {
+        const result = await fetch('/api/review', {
+            method: "POST",
+            body: JSON.stringify(obj)
+        })
+
+        const data = await result.json()
+        console.log("data==>", data)
+        if (data.status === "success") {
+            router.refresh()
+        }
+
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input placeholder="John Doe" {...field} />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="review"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Review</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Your course is awesome!" {...field} />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+
+
 }
 
 const SubmitButton = (props: ComponentProps<typeof Button>) => {
